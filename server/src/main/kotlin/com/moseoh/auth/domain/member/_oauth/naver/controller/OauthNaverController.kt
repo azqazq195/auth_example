cea319ba3dff2fc9120350f49d105c275fd1bc6b
moseoh <azqazq195@gmail.com>
2023-01-28 07:29:58 +0900

    feat: 기본 폴더 구조 및 설정 추가

---
+++ server/src/main/kotlin/com/moseoh/auth/domain/member/_oauth/naver/controller/OauthNaverController.kt
@@ -0,0 +1,9 @@
+package com.moseoh.auth.domain.member._oauth.naver.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/v1/member/oauth/naver")
+class OauthNaverController {
+}
